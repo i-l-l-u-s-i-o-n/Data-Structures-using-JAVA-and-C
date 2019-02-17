bcec51b3db3cdc1a1047671b1e5d16acb86342c2
Shivam1097 <shivam.dev1097@gmail.com>
2019-02-17 10:34:13 +0530

    Currently working

---
+++ Singly LinkedList/src/com/shivam/Main.java
@@ -0,0 +1,30 @@
+package com.shivam;
+
+
+
+
+/*
+
+ Advantages of Linked list ->
+---------------------------------------
+
+ We can dynamically add a new element because of no fixed size as in array.
+
+ we can also add element at front without shifting elements as in arrays.
+
+ Disadvantage of linked list ->
+----------------------------------------
+
+ Slow in accessing elements ,  and we can't access any no. randomly but in array, random access of number is possible by using INDEX.
+ */
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Singly LinkedList/src/com/shivam/Node.java
@@ -0,0 +1,7 @@
+package com.shivam;
+
+/**
+ * created by Shivam on 17-02-2019.
+ */
+public class Node {
+}
