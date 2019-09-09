d195e15c2daeccb84e9bf9a79528e393a0dc2466
Shivam1097 <shivam.dev1097@gmail.com>
2019-09-09 19:24:53 +0530

    Initial Commit

---
+++ Data Structures Using C/Doubly Linked List.c
@@ -0,0 +1,92 @@
+
+
+/*
+	Name: Doubly Linked List
+	Author: Shivam Shukla
+	Date: 09-09-19 19:23
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct dl{
+	struct dl * prev;
+	int data;
+	struct dl * next;
+} node;
+
+node *head =NULL;
+
+void insertAtBeg(int item){
+	
+	node *ptr =(node *)malloc(sizeof(node));
+	
+	ptr->data=item;
+	ptr->prev=NULL;
+	
+	if(head==NULL){
+		ptr->next=NULL;
+		head=ptr;
+	}else{
+		ptr->next=head;
+		head->prev=ptr;
+		head=ptr;
+	}
+}
+
+
+void deleteFromLast(){
+	
+	if(head==NULL){
+		printf("EMPTY!!");
+		return;
+	}
+	
+	node *temp=head;
+	while(temp->next!=NULL){
+		temp=temp->next;
+	}
+	node * ptr = temp;
+	temp->prev->next=NULL;
+	temp->prev=NULL;
+	
+	free(ptr);
+}
+
+void display(){
+	if(head==NULL){
+		printf("EMPTY!!");
+		return;
+	}
+	
+	printf("\n");
+	node *temp=head;
+	while(temp!=NULL){
+		printf("%d\t",temp->data);
+		temp=temp->next;
+	}
+	
+}
+
+
+
+int main(){
+	
+	insertAtBeg(700);
+	insertAtBeg(78);
+	insertAtBeg(60);
+	insertAtBeg(71);
+	insertAtBeg(75);
+	insertAtBeg(76);
+	
+	display();				// 76      75      71      60      78      700
+
+	
+	deleteFromLast();
+	
+	display();				// 76      75      71      60      78
+	
+}
+
+
